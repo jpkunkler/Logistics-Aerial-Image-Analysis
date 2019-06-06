8a7dcb155b80ae0b16998fb7096f794e8305477d
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2019-06-06 13:12:51 +0200

    added interactive web app

---
+++ web/v2/app/server.py
@@ -0,0 +1,79 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from torch import topk
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+from PIL import Image
+import urllib.request
+
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_name = 'export.pkl'
+
+classes = ['Gut', 'Mittel', 'Schlecht', 'Sehr_Gut']
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+
+
+async def setup_learner():
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+
+@app.route('/')
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    with urllib.request.urlopen(img_data["file"]) as url:
+        f = BytesIO(url.read())
+    img = open_image(f)
+    prediction = learn.predict(img)
+    prob, label = topk(prediction[2],len(classes))
+    inv_map = {v: k for k, v in learn.data.c2i.items()}
+    labels = list(map(inv_map.get,label.data.tolist()))
+    prob = [round(float(x),2) for x in prob.data.tolist()]
+    output = dict(zip(labels, prob))
+    output.update({'result': str(prediction[0])})
+    return JSONResponse(output)
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
---
+++ web/v2/app/static/client.js
@@ -0,0 +1,46 @@
+var el = x => document.getElementById(x);
+
+function showPicker() {
+  el("file-input").click();
+}
+
+function showPicked(input) {
+  el("upload-label").innerHTML = input.files[0].name;
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    el("image-picked").src = e.target.result;
+    el("image-picked").className = "";
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+
+function analyze() {
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+
+  el("analyze-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Eingestuft als ${response["result"]}`;
+      el("result-probabilities").innerHTML = `Mit folgenden Wahrscheinlichkeiten:`;
+      el("probability-verygood").innerHTML = `Sehr gut: ${response["Sehr_Gut"]}`;
+      el("probability-good").innerHTML = `Gut: ${response["Gut"]}`;
+      el("probability-average").innerHTML = `Mittel: ${response["Mittel"]}`;
+      el("probability-bad").innerHTML = `Schlecht: ${response["Schlecht"]}`;
+    }
+    el("analyze-button").innerHTML = "Analyze";
+  };
+
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
+}
+
